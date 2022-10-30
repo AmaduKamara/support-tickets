e8f5bfc89a8e3aa68bf45e3c8572a79db657f9c9
Amkam <amaduskamara@gmail.com>
2022-10-30 21:02:32 +0000

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userController");
+
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", registerUser);
 
 router.post("/login", loginUser);
+
+// Protected Route
+router.get("/me", protect, getMe);
 
 module.exports = router;
