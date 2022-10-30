560580c2980fb826ecd1c28965635b3f3d76bb07
Amkam <amaduskamara@gmail.com>
2022-10-30 21:02:29 +0000

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,17 +5,21 @@ const userSchema = mongoose.Schema(
     name: {
       type: String,
       required: [true, "Please add a name"],
+      trim: true,
     },
     email: {
       type: String,
       required: [true, "Please add an email"],
+      trim: true,
     },
     password: {
       type: String,
       required: [true, "Please add a password"],
+      trim: true,
     },
     isAdmin: {
       type: Boolean,
+      // required: true,
       dafault: false,
     },
   },
