b287f12bb50354f6317a8427c117c25f77b76e76
Amkam <amaduskamara@gmail.com>
2022-10-30 14:04:01 +0000

    Use express async handler to wrap async functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,27 @@
-const registerUser = (req, res) => {
-  res.send("Register Routes");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+// @Dec     Register a new user
+// @Route   /api/users
+// @Access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  // destructure the user data from the req body
+  const { name, email, password } = req.body;
+
+  // validate data sent from the body
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+  console.log(req.body);
+  res.send("Register Routes");
+});
+
+// @Dec     Login a user
+// @Route   /api/users/login
+// @Access  Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send("Login Route");
-};
+});
 
 module.exports = {
   registerUser,
