df36452c3463431516eacefb12b6fc4f2fe013f9
Amkam <amaduskamara@gmail.com>
2022-10-30 13:04:35 +0000

    Add express config and server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Server backend");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Routes
+app.get("/", (req, res) => {
+  res
+    .status(201)
+    .json({ message: "Welcome to the MERN Stack Support Tickets system" });
+});
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}}`));
