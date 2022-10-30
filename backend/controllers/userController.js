14ab4a6e5f25c05c574026b6cdc323855bc366ad
Amkam <amaduskamara@gmail.com>
2022-10-30 20:17:12 +0000

    Register, log, and generate a token for a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // User Model
 const User = require("../models/userModel");
@@ -8,27 +9,27 @@ const User = require("../models/userModel");
 // @Route   /api/users
 // @Access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  // destructure the user data from the req body
+  // 1. destructure the user data from the req body
   const { name, email, password } = req.body;
 
-  // validate data sent from the body
+  // 2. validate data sent from the body
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please include all fields");
   }
 
-  // Find if user already exist
+  // 3. Find if user already exist
   const userExist = await User.findOne({ email });
   if (userExist) {
     res.status(400);
     throw new Error("User already exists");
   }
 
-  // Hash password
+  // 4. Hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  // Create user
+  // 5. Create user
   const user = await User.create({ name, email, password: hashedPassword });
 
   if (user) {
@@ -36,6 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id), // generate a tokan and pass it to a user that's being registered
     });
   } else {
     res.status(400);
@@ -47,8 +49,32 @@ const registerUser = asyncHandler(async (req, res) => {
 // @Route   /api/users/login
 // @Access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send("Login Route");
+  // 1. destructure the user data credentials the req body
+  const { email, password } = req.body;
+
+  // 2. Find user by email
+  const user = await User.findOne({ email });
+
+  // Check if password matches
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id), // generate a tokan and pass it to a user that's loggedin
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
 });
+
+// Generate a token for a user for when registered and loggedin
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
