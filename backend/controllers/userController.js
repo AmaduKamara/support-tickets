e273fc7ac14615e8109cfce58ddfb49bc08b7709
Amkam <amaduskamara@gmail.com>
2022-10-30 19:31:53 +0000

    Create user functionality and hashing password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+// User Model
+const User = require("../models/userModel");
 
 // @Dec     Register a new user
 // @Route   /api/users
@@ -12,8 +16,31 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please include all fields");
   }
-  console.log(req.body);
-  res.send("Register Routes");
+
+  // Find if user already exist
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @Dec     Login a user
