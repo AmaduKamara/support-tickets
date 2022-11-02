eba9a599a9839f1dee2050e5895d67ca50a9074e
Amkam <amaduskamara@gmail.com>
2022-11-02 23:49:59 +0000

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { register } from "../features/auth/authSlice";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,6 +13,11 @@ const Register = () => {
   });
 
   const { name, email, password, password2 } = formData;
+  const dispatch = useDispatch();
+
+  const { user, isSuccess, isLoading, isError, message } = useSelector(
+    (state) => state.auth
+  );
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -23,6 +30,14 @@ const Register = () => {
     e.preventDefault();
     if (password !== password2) {
       toast.error("Passwords do not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
     }
   };
 
@@ -30,10 +45,10 @@ const Register = () => {
     <div className="container mx-auto py-16 flex justify-center md:w-4/5 lg:w-1/2">
       <section className="px-16 w-full md:w-4/5">
         <div className="text-center">
-          <h1 className="flex items-center justify-center text-4xl font-bold text-gray-600">
+          <h1 className="flex items-center justify-center text-2xl md:text-4xl font-bold text-gray-600">
             <FaUser className="mr-2 text-gray-500" /> Register
           </h1>
-          <p className="text-2xl mt-5 font-bold text-gray-400">
+          <p className="text-lg md:text-2xl mt-5 font-bold text-gray-400">
             Please create an account
           </p>
         </div>
