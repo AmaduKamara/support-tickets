587d054b061af6db7b8394453b521f06781fd165
Amkam <amaduskamara@gmail.com>
2022-11-02 23:49:49 +0000

    Add login action

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -8,6 +8,19 @@ const initialState = {
   message: "",
 };
 
+// Register new user
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    console.log(user);
+  }
+);
+
+// Login user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  console.log(user);
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
