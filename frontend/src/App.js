b7826c26415c599d0bcbe71e70649cc2811d0b2d
Amkam <amaduskamara@gmail.com>
2022-11-02 22:29:02 +0000

    Use react toastify library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+  import { ToastContainer } from "react-toastify";
+  import "react-toastify/dist/ReactToastify.css";
+
+// Components imports
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -17,6 +21,7 @@ const App = () => {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 };
