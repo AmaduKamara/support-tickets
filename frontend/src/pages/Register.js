7d588f9c766ae966723689437ca292a1c7f68997
Amkam <amaduskamara@gmail.com>
2022-11-02 18:04:17 +0000

    Create Register.js

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="container mx-auto py-16">
+      <h1>Register</h1>
+    </div>
+  );
+};
+
+export default Register;
