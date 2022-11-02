eef2fd12838f7ad2d6132b32d08e50b2bb45ea19
Amkam <amaduskamara@gmail.com>
2022-11-02 18:04:04 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const App = () => {
   return (
-    <div>
-      <h1>MERN Support Tickes app</h1>
-    </div>
+    <>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
