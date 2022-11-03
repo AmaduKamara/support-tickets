bd8ad1d8125f70b5c0bfe129f523db83cefea3cd
Amkam <amaduskamara@gmail.com>
2022-11-03 01:32:03 +0000

    Update authService.js

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -10,12 +10,22 @@ const register = async (userData) => {
   return response.data;
 };
 
+// Log user in
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "/login", userData);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
 // Log user out
 const logout = () => localStorage.removeItem("user");
 
 const authService = {
   register,
   logout,
+  login,
 };
 
 export default authService;
