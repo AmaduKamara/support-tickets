b073f8d9846ddad29dae5f87e1fdab3255420db5
Amkam <amaduskamara@gmail.com>
2023-10-04 15:17:56 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 // Accept urlencoded boday data
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res
